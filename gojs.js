91fcfeb0492597ebaf73df82fb43b44e78a47d32
Sorrawat Wisutthiwan <mp-golf@hotmail.com>
2021-03-05 00:28:12 +0700

    add button and highlight

--- gojs.js
+++ gojs.js
@@ -26,6 +26,10 @@ function init() {
 
   diagram.nodeTemplate = $(
     go.Node,
+    {
+      fromSpot: go.Spot.Bottom, // coming out from middle-right
+      toSpot: go.Spot.Top,
+    },
     "Auto",
     { desiredSize: new go.Size(75, 75) },
     new go.Binding("location", "loc", go.Point.parse).makeTwoWay(
@@ -37,6 +41,7 @@ function init() {
       { isClipping: true },
       $(go.Shape, "Circle", { width: 75, strokeWidth: 0 }),
       $(
+        
         go.Picture,
         { margin: 10, width: 75, height: 75 },
         new go.Binding("source", "", choosePic)
@@ -44,14 +49,65 @@ function init() {
     )
   );
 
+  diagram.linkTemplate = $(
+    go.Link,
+    {
+      routing: go.Link.AvoidsNodes, // Orthogonal routing
+      corner: 10, // with rounded corners
+      curve: go.Link.JumpOver,
+    },
+    new go.Binding("fromEndSegmentLength"),
+    new go.Binding("toEndSegmentLength"),
+    new go.Binding("points").makeTwoWay(),
+    $(go.Shape),
+    $(
+      go.Shape, // the "to" end arrowhead
+      { toArrow: "Standard" }
+    )
+  );
+
+  highlighter = $(
+    go.Part,
+    "Auto",
+    {
+      layerName: "Background",
+      selectable: false,
+      isInDocumentBounds: false,
+      locationSpot: go.Spot.Center,
+    },
+    $(go.Shape, "Circle", {
+      fill: $(go.Brush, "Radial", { 0.4: "#DBFF00", 1.0: "#FFFFFF" }),
+      stroke: null,
+      desiredSize: new go.Size(110, 110),
+    })
+  );
+  diagram.add(highlighter);
+
+  // diagram.layout =
+  // $(go.LayeredDigraphLayout,  // this will be discussed in a later section
+  //   { columnSpacing: 5,
+  //     setsPortSpots: false });
+
   var nodeDataArray = [
-    { key: 1, loc: "0 0", isHighlighted: true, name: "H" }, //, "name": "Start\nstate"
+    { key: 1, loc: "0 0", isHighlighted: false, name: "HH" }, //, "name": "Start\nstate"
     { key: 2, loc: "400 0", isHighlighted: false, name: "O" }, //, "name": "End\nstate"
-    { key: 3, loc: "200 200", name: "start" },
+    { key: 3, loc: "200 -200", isHighlighted: false, name: "start" },
   ];
-  var linkDataArray = [{ from: 1, to: 2 }, { from: 1, to: 3 }];
+  var linkDataArray = [
+    { from: 3, to: 1 },
+    {
+      from: 3,
+      to: 2,
+      //   points: [200, -200,300, -100,400,0],
+    },
+  ];
 
   diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+  var node = diagram.findNodeForKey(1);
+  highlighter.location = new go.Point(
+    node.location.x + 38,
+    node.location.y + 38
+  );
 }
 
 function choosePic(data) {
